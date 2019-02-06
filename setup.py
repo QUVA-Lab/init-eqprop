7e4213fb7a0f3a93c3eea790f3e7785ead56c4a1
Peter O'Connor <peter.ed.oconnor@gmail.com>
2019-02-06 14:52:59 +0000

    updated requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ setup(
     name = "init-eqprop",
     author = "peter",
     version = 0,
-    install_requires = ['numpy', 'matplotlib', 'torch', 'torchvision', 'plato', 'artemissss'],
-    dependency_links = [
-        "http://github.com/petered/plato/tarball/ongoing_changes_2#egg=plato",
-        "http://github.com/quva-lab/artemis/tarball/peter#egg=artemissss",
-    ],
+    install_requires = ['numpy', 'matplotlib', 'torch', 'torchvision'],
+    dependency_links = [],
     scripts = [],
     packages=find_packages(),
     )
